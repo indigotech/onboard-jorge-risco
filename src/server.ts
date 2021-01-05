55244338fcea352a2e8f230d29783b15c8f6a9f3
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-05 20:52:52 -0300

    added Apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!")
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String,
+    goodbye: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    goodbye:() => 'Goodbye world!'
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
