7b0efa275c8911a006d9b2d70350812bc4df5c80
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-08 18:53:11 -0300

    added login mutation

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,37 @@ const typeDefs = gql`
     hello: String
     goodbye: String
   }
+
+  type User {
+    id: ID
+    name: String
+    email: String
+    birthDate: String
+    cpf: String
+  }
+
+  type LoginResponse {
+    user: User
+    token: Int
+  }
+
+  type Mutation {
+    login(email: String, password: String): LoginResponse
+  }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
     goodbye: () => 'Goodbye world!',
+  },
+  Mutation: {
+    login(_, args) {
+      return {
+        user: { id: 123, birthDate: '2020-01-01', email: args.email, cpf: '62693406080' },
+        token: 12,
+      };
+    },
   },
 };
 
