9f82c0f807591dffa896f853e6100da9921a1218
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-11 13:41:46 -0300

    added mock_user.ts according to the 'User' type located in src/graphql/typedefs.ts

---
+++ src/mock_user.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { User } from './entity/User';
+
+createConnection()
+  .then(async (connection) => {
+    console.log('Inserting a new user into the database...');
+    const user = new User();
+    user.name = 'Fulano';
+    user.email = 'fulano@email.com';
+    user.birthDate = '1444-01-01';
+    user.cpf = '0000000001';
+    await connection.manager.save(user);
+    console.log(`Saved a new user with id: ${user.id} `);
+
+    console.log('Loading users from the database...');
+    const users = await connection.manager.find(User);
+    console.log('Loaded users: ', users);
+
+    console.log('Here you can setup and run express/koa/any other framework.');
+  })
+  .catch((error) => console.log(error));
