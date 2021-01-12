4afef699436e75b41b47f278bead423490cf9a2f
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-12 09:41:04 -0300

    changed variable name according to the camelCase pattern

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,5 +1,5 @@
 export function hash(data: string, salt: string): string {
-  const unhashed_string = data + salt;
-  const hashed_string: string = require('crypto').createHash('sha256').update(unhashed_string).digest('base64');
-  return hashed_string;
+  const unhashedString = data + salt;
+  const hashedString: string = require('crypto').createHash('sha256').update(unhashedString).digest('base64');
+  return hashedString;
 }
