d1f255e8c0c75081cc29171454853f49feceb182
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-12 09:44:23 -0300

    updated crypto.ts
    declared the imports in the beginning of the file

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,5 +1,7 @@
+import * as crypto from 'crypto';
+
 export function hash(data: string, salt: string): string {
   const unhashedString = data + salt;
-  const hashedString: string = require('crypto').createHash('sha256').update(unhashedString).digest('base64');
+  const hashedString: string = crypto.createHash('sha256').update(unhashedString).digest('base64');
   return hashedString;
 }
