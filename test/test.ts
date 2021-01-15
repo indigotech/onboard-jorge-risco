82e92d436920512fd05dd3c9a19426967275d125
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-15 15:43:54 -0300

    added login mutation test

--- test/test.ts
+++ test/test.ts
@@ -2,11 +2,15 @@ import * as request from 'supertest';
 import { runServer } from '../src/server';
 import { expect } from 'chai';
 import { checkToken } from '../src/crypto';
+import { getRepository, Repository } from 'typeorm';
+import { User } from '../src/entity/User';
 const url = `http://localhost:4000/`;
 
+let usersRepo: Repository<User>;
 before(async () => {
   try {
     await runServer();
+    usersRepo = getRepository(User);
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
@@ -22,6 +26,23 @@ describe('Query test', () => {
 
     const response = await request(url).post('').send(query);
     expect(response.body.data.hello).to.be.eq('Hello world!');
+  });
+});
+
+describe('Login Mutation test', async () => {
+  beforeEach(async () => {
+    const newUser = usersRepo.create({
+      name: 'Fulano',
+      email: 'fulano@email.com',
+      birthDate: '1444-01-01',
+      cpf: '0',
+      password: 'AwnLfl1zsnvUDHeXnhKHFfUjTekcoXRDp6336GRSWRg=',
+    });
+    await usersRepo.save(newUser);
+  });
+
+  afterEach(async () => {
+    usersRepo.clear();
   });
 
   it('should return user "Fulano"', async () => {
