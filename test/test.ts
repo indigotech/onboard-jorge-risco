ad78e0fccd7ba5c40872c89c745570f037ba7946
Jorge M. Risco <jorgedelano7@gmail.com>
2021-01-15 14:39:11 -0300

    token validation test

--- test/test.ts
+++ test/test.ts
@@ -3,6 +3,15 @@ import { runServer } from '../src/server';
 import { expect } from 'chai';
 
 const url = `http://localhost:${process.env.SERVERPORT}/`;
+import { checkToken } from '../src/crypto';
+
+before(async () => {
+  try {
+    await runServer();
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+});
 
 describe('Query test', () => {
   before(async () => {
@@ -23,17 +32,19 @@ describe('Query test', () => {
     expect(response.body.data.hello).to.be.eq('Hello world!');
   });
 
-  it('should return user "Siclano"', async () => {
+  it('should return user "Fulano"', async () => {
     const query = {
       query: `mutation{
-        login(email:"siclano@email.com", password: "best_password_ever"){
+        login(email:"fulano@email.com", password: "dumb_password", rememberMe: true){
           user{
             name
-          }
+          },
+          token
         }
       }`,
     };
     const response = await request(url).post('').send(query);
-    expect(response.body.data.login.user.name).to.be.eq('Siclano');
+    checkToken(response.body.data.login.token);
+    expect(response.body.data.login.user.name).to.be.eq('Fulano');
   });
 });
